6917b37f4f8b08c60c65c975af29eb262c77d675
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2020-03-19 22:17:57 +0100

    [TASK] migrate TCA from ext_tables.php to Configuration/TCA/Overrides

--- ext_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die('Access denied.');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-	'rte_ckeditor_dl',
-	'Configuration/TsConfig/Page/definitionlists.ts',
-	'RTE: definition lists'
-);
